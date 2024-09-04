0696f2d34701df030ea8412294f37765e7f91f7e
0xMrPanda <meetpandya.dev@gmail.com>
2024-09-04 19:40:30 +0530

    Refactored the code to make it more robust to be deployable on other chains.

---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+// Deploy mocks when we are on local anvil chain
+// keep tack of contract address across different chains
+
+import {Script} from "forge-std/Script.sol";
+
+contract HelperConfig {
+    NetworkConfig public activeNetworkConfig;
+
+    struct NetworkConfig {
+        address priceFeed; // ETH/USD Price Feed Address
+    }
+
+    constructor() {
+        if (block.chainid == 11155111) {
+            activeNetworkConfig = getSepoliaEthConfig();
+        } else {
+            activeNetworkConfig = getAnvilEthConfig();
+        }
+    }
+
+    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
+        // price feed address
+        NetworkConfig memory sepoliaConfig = NetworkConfig({
+            priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        });
+        return sepoliaConfig;
+    }
+
+    function getAnvilEthConfig() public pure returns (NetworkConfig memory) {
+        //price feed address
+    }
+}
